195ddb419314791f99942be31b2445e168ffd082
fsimchen <felipe.simchen@gmail.com>
2020-10-11 22:09:09 -0300

    commit

--- install.sh
+++ install.sh
@@ -31,13 +31,16 @@ echo "Setting AUR repository mirrorlist . . ."
 yay -S --noconfirm chaotic-mirrorlist
 yay -S --noconfirm chaotic-keyring
 echo '' | sudo tee -a /etc/pacman.conf
+echo '[chaotic-aur]' | sudo tee -a /etc/pacman.conf
+echo 'SigLevel = Never' | sudo tee -a /etc/pacman.conf
+echo 'Include = /etc/pacman.d/chaotic-mirrorlist' | sudo tee -a /etc/pacman.conf
+echo '' | sudo tee -a /etc/pacman.conf
 echo '[andontie-aur]' | sudo tee -a /etc/pacman.conf
 echo 'SigLevel = Never' | sudo tee -a /etc/pacman.conf
 echo 'Server = https://aur.andontie.net/$arch' | sudo tee -a /etc/pacman.conf
 echo '' | sudo tee -a /etc/pacman.conf
-echo '[chaotic-aur]' | sudo tee -a /etc/pacman.conf
-echo 'SigLevel = Never' | sudo tee -a /etc/pacman.conf
-echo 'Include = /etc/pacman.d/chaotic-mirrorlist' | sudo tee -a /etc/pacman.conf
+echo '[sublime-text]' | sudo tee -a /etc/pacman.conf
+echo 'Server = https://download.sublimetext.com/arch/stable/x86_64' | sudo tee -a /etc/pacman.conf
 echo '' | sudo tee -a /etc/pacman.conf
 sudo pacman -Syy
 
